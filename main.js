7ecfdc46ff2d962ba66a2229a9d59f7ad36edfc7
Barbagris <141267073+Barbagris@users.noreply.github.com>
2023-09-28 11:56:14 -0300

    Add files via upload

--- main.js
+++ main.js
@@ -1,1 +1,14 @@
-console.log("Hello world!");
+// Necesitamos una función que haga que la compu elija entre las tres opcione
+// Armar los input para que el usuario pueda ingresar su opción
+// Armar los output a partir de condicionales en los que salga quien gana 
+// Preparar un contador que lleve el conteo y defina al ganador
+
+function getcomputerchoice(decision){
+    function random(number) {
+        return Math.floor(Math.random()*3);
+      }
+    if 
+    else if
+    else if
+}
+
