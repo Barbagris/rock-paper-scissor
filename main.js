97a754e933993ddbd2ca1f760f8ae0504bf41269
Barbagris <141267073+Barbagris@users.noreply.github.com>
2023-10-01 20:45:33 -0300

    Add files via upload

--- main.js
+++ main.js
@@ -3,12 +3,28 @@
 // Armar los output a partir de condicionales en los que salga quien gana 
 // Preparar un contador que lleve el conteo y defina al ganador
 
-function getcomputerchoice(decision){
+let 
+
+function getComputerChoice(){
     function random(number) {
-        return Math.floor(Math.random()*3);
+        return Math.floor(Math.random()*number);
       }
-    if 
-    else if
-    else if
+    let number=random(3)+1
+    if (number===1){
+      return 'Rock';
+    }
+    else if (number===2) {
+      return 'Paper';
+    }
+    else if (number===3) {
+      return 'Scissors';
+    }
 }
 
+
+const PlayerSelectiont=prompt('Rock, Paper or Scissors?')
+
+function playRound(getComputerChoice,PlayerSelectiont){
+
+
+}
