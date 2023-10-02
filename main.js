f3a13120ff9c4098898cac04666e54bd1e7fa653
Barbagris <141267073+Barbagris@users.noreply.github.com>
2023-10-01 22:21:50 -0300

    Trabajo sobre Game

--- main.js
+++ main.js
@@ -25,17 +25,48 @@ function playRound(getComputerChoice,PlayerSelectiont){
   let playerChoice= PlayerSelectiont.toLowerCase();
   let computerChoice=getComputerChoice().toLowerCase()
   if (playerChoice==='rock' && computerChoice==='scissors'){
-    return console.log('You win! Rock beats Scissors');
+    return 'You win! Rock beats Scissors';
   }
   else if (playerChoice==='scissors' && computerChoice==='paper'){
-    return console.log('You win! Scissors beats Paper');
+    return 'You win! Scissors beats Paper';
   }
   else if (playerChoice==='paper' && computerChoice==='rock'){
-    return console.log('You win! Paper beats Rock');
+    return 'You win! Paper beats Rock';
   }
-  else {
-    console.log('You lose!');
+  else if(playerChoice==='paper' && computerChoice==='scissors') {
+    return'You lose! Scissors beats Paper';
+  }
+  else if(playerChoice==='scissors' && computerChoice==='rock') {
+    return'You lose! Rock beats Scissors';
+  }
+  else if(playerChoice==='rock' && computerChoice==='paper') {
+    return'You lose! Paper beats Rock';
+  }
+  else if (playerChoice=computerChoice) {
+    return "It's a tie try again"
   }
 }
 
-playRound(getComputerChoice, PlayerSelectiont);
+const resultado = playRound(getComputerChoice, PlayerSelectiont);
+
+
+function game (){
+  let playerPuntuation = 0
+  let computerPuntuation = 0 
+  function roundcounter (){
+      if (resultado='You lose! Scissors beats Paper' || 'You lose! Rock beats Scissors' || 'You lose! Scissors beats Paper'){
+        counter = (computerPuntuation)+1
+      }
+      else if (resultado='You win! Scissors beats Paper' || 'You win! Rock beats Scissors' || 'You win! Scissors beats Paper'){
+        counter = (playerPuntuation)+1
+      }
+    }
+  if (playerPuntuation=3){
+    console.log('YOU WIN THE GAME!');
+  }
+  else if (computerPuntuation=3){
+    console.log('YOU LOSE THE GAME!');
+  }
+}
+
+//Buscar como funcionan los for en la proxima leccion y ver como incluirlo para que funcione.
